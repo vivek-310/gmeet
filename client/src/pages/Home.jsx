8c31f4602c62b16a63b0eb056265fe0012c99607
vivek-310 <vivekvardhan0310@gmail.com>
2025-04-22 23:32:06 +0530

    home updates

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Navigate, useNavigate } from 'react-router-dom';
+import{v4 as uuid} from 'uuid';
 
 function Home() {
+  const navigate=useNavigate();
+
+  const handlecreateRoom=()=>{
+    const id=uuid();
+    navigate(`/room/${id}`)
+  }
+
   return (
-    <div>Home</div>
+    <div>
+      <div className='h-screen flex flex-col items-center justify-center'>
+        <h1 className='font-bold text-2xl'>welcome to google meet</h1>
+        <button onClick={handlecreateRoom} className='bg-blue-500 text-white p-4 mt-4 rounded-2xl text-xl font-medium'>create and start the meet</button>
+      </div>
+    </div>
   )
 }
 
