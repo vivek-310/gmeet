00dbcfc03924a8b5759be5c6cc933150dbf88882
narayana <beearamnarayan2002@mail.com>
2025-04-22 21:51:01 +0530

    server update

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const cors = require('cors');
+const app= express();
+app.use(cors());
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+} 
+);
