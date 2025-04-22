2372c79ef73f06fe9bfa2ad45c496cc3d3617091
vivek-310 <vivekvardhan0310@gmail.com>
2025-04-22 22:04:03 +0530

    route added

--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@ const cors = require('cors');
 const app= express();
 app.use(cors());
 
+app.get('/',(req,res)=>{
+    res.send("hello");
+})
+
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 } 
