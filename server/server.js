965dd8401c535c3329ae5dce213850fcda01a7c7
narayana <beearamnarayan2002@mail.com>
2025-05-02 21:54:43 +0530

    minivideo

--- server/server.js
+++ server/server.js
@@ -47,6 +47,27 @@ io.on('connection', (socket) => {
         });
     });
 
+    socket.on('offer', (data) => {
+        socket.to(data.target).emit('offer', {
+            sdp: data.sdp,
+            sender: socket.id,
+        });
+    });
+
+    socket.on('answer', (data) => {
+        socket.to(data.target).emit('answer', {
+            sdp: data.sdp,
+            sender: socket.id,
+        });
+    });
+
+    socket.on('ice-candidate', (data) => {
+        socket.to(data.target).emit('ice-candidate', {
+            candidate: data.candidate,
+            sender: socket.id,
+        });
+    });
+
     socket.on('disconnect', () => {
         console.log("user disconnected", socket.id);
     });
